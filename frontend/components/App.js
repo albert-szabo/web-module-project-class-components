e1a26cbf5845e9663179e8d39b4f6e9add978b7c
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-09 22:36:36 -0700

    Added state for tasks and laid the groundwork for completed toggle

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,22 +1,57 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      toDoItems: [
+        {
+          name: 'Organize garage',
+          id: 1,
+          completed: false
+        },
+        {
+          name: 'Bake cookies',
+          id: 2,
+          completed: false
+        },
+        {
+          name: 'Feed the cat',
+          id: 3,
+          completed: false
+        },
+        {
+          name: 'Take out the trash',
+          id: 4,
+          completed: false
+        },
+        {
+          name: 'Go for a walk',
+          id: 5,
+          completed: false
+        }
+      ]
+    }
+  }
+
   render() {
+    const { toDoItems } = this.state;
+
     return (
       <div>
         <h1>To Do App</h1>
 
         <ul>
-          <li>Feed the cat</li>
-          <li>Take out the trash</li>
-          <li>Go for a walk</li>
+          {toDoItems.map(item => {
+            return (<li>{item.name} {item.completed ? <span>-completed</span> : <span></span>}</li>)
+          })}
         </ul>
 
         <form>
           <input/>
           <button>Add</button>
         </form>
-        
+
         <button>Clear</button>
       </div>
     )
