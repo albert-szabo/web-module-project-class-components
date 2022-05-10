4ba5fd69f81b520b5c4ad8a47b9a3514db5d7290
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-10 00:54:23 -0700

    Fixed bug in task completion toggle functionality

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default class Todo extends React.Component {
   handleClick = () => {
-    this.props.handleToggle(this.props.todo.id);
+    this.props.handleToggle(this.props.task.id);
   }
-  
+
   render() {
     return (<li onClick={this.handleClick}>{this.props.task.name} {this.props.task.completed ? <span>-completed</span> : <span></span>}</li>);
   }
