20ad688e4a9666d9de1eb1af168885c04b5d7750
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-09 22:16:00 -0700

    Completed initial DOM construction

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,20 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>To Do App</h1>
+
+        <ul>
+          <li>Feed the cat</li>
+          <li>Take out the trash</li>
+          <li>Go for a walk</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+        
+        <button>Clear</button>
       </div>
     )
   }
